8fc61886ee3c4c6c64407a8923b1ec1fec03dee3
Roberto Angulo <89711998+RobertoAnguloDeveloper@users.noreply.github.com>
2024-09-09 16:30:36 -0500

    user changed

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:plg-cmms-2024@localhost/flasktest'
+        'postgresql://rangulot:plg-cmms-2024@localhost/flasktest'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
