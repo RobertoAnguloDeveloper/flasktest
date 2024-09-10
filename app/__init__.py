a5ea40b9c7357550807a86d65b11b180c991ff8f
Roberto Angulo <89711998+RobertoAnguloDeveloper@users.noreply.github.com>
2024-09-09 20:09:16 -0500

    Upload photo

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 migrate = Migrate()
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
@@ -13,8 +14,10 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    csrf.init_app(app)
 
-    from .routes import main
-    app.register_blueprint(main)
+    with app.app_context():
+        from . import routes
+        app.register_blueprint(routes.main)
 
     return app
