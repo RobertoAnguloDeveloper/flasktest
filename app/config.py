9ce3cd0da56b99735eb878aeee8baa8c7e7133b1
Roberto Angulo <89711998+RobertoAnguloDeveloper@users.noreply.github.com>
2024-09-09 20:07:08 -0500

    Upload photo

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Angulo73202647'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://rangulot:plg-cmms-2024@localhost/flasktest'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
